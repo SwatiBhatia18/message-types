9e33630fb2cc2a153a1cdc9edb9437d33295293c
Vivek Vaibhav <vivek@oriserve.com>
2022-06-22 18:08:28 +0530

    btn import issue fixed

--- src/messagetypes/common/PromptMsg/PromtMsgBody/index.js
+++ src/messagetypes/common/PromptMsg/PromtMsgBody/index.js
@@ -2,7 +2,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-import Buttons from '../../../components/buttons'
+import Buttons from '../../../../components/buttons'
 
 const PromptMsgBody = ({
   payload,
