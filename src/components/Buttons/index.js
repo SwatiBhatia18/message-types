32e781ee6bb52dd3b41f81cdee298a477d617b0e
Vivek Vaibhav <vivek@oriserve.com>
2022-06-27 16:48:07 +0530

    code formatted

--- src/components/Buttons/index.js
+++ src/components/Buttons/index.js
@@ -18,9 +18,8 @@ class Buttons extends React.PureComponent {
 
   handleBtnClick = button => {
     const { handleMsgBtnClick, message } = this.props
-    if (handleMsgBtnClick)
-      handleMsgBtnClick({ button, message })
-  }
+    if (handleMsgBtnClick) handleMsgBtnClick({ button, message })
+  };
 
   showAllButtons = () => {
     const { buttons } = this.props
@@ -28,7 +27,7 @@ class Buttons extends React.PureComponent {
       show_all_buttons: true,
       display_buttons_count: buttons.length
     })
-  }
+  };
 
   showLessButtons = () => {
     const { display_count } = this.props
@@ -36,35 +35,38 @@ class Buttons extends React.PureComponent {
       show_all_buttons: false,
       display_buttons_count: display_count
     })
-  }
+  };
 
   render() {
     const { btn_disabled, buttons, className } = this.props
     const { display_buttons_count, show_all_buttons } = this.state
     return (
-      <div className={`${styles.buttonsContainer} ${className} buttonsContainer`}>
-        {
-          buttons.map((btn, index) => {
-            if (index < display_buttons_count)
-              return (
-                <Button
-                  key={index}
-                  size="small"
-                  className={`ori-b-mrgn-5 ${btn.displayType === "paragraph" ?
-                      "ori-btn-paragraph" :
-                      (btn.displayType === "secondary" ? "ori-r-mrgn-5 ori-btn-secondary" : "ori-r-mrgn-5 ori-btn-bubble-inner")
-                    } ${styles.button}`}
-                  block={btn.displayType === "paragraph"}
-                  disabled={btn_disabled || btn.disabled}
-                  onClick={this.handleBtnClick.bind(this, btn)}
-                >
-                  {btn.text}
-                </Button>
-              )
-          })
-        }
-        {
-          !show_all_buttons && buttons.length > display_buttons_count &&
+      <div
+        className={`${styles.buttonsContainer} ${className} buttonsContainer`}
+      >
+        {buttons.map((btn, index) => {
+          if (index < display_buttons_count) {
+            return (
+              <Button
+                key={index}
+                size="small"
+                className={`ori-b-mrgn-5 ${
+                  btn.displayType === 'paragraph'
+                    ? 'ori-btn-paragraph'
+                    : btn.displayType === 'secondary'
+                      ? 'ori-r-mrgn-5 ori-btn-secondary'
+                      : 'ori-r-mrgn-5 ori-btn-bubble-inner'
+                } ${styles.button}`}
+                block={btn.displayType === 'paragraph'}
+                disabled={btn_disabled || btn.disabled}
+                onClick={this.handleBtnClick.bind(this, btn)}
+              >
+                {btn.text}
+              </Button>
+            )
+          }
+        })}
+        {!show_all_buttons && buttons.length > display_buttons_count && (
           <Button
             size="small"
             className={`ori-b-mrgn-5 ori-r-mrgn-5 ori-btn-bubble-inner ${styles.button}`}
@@ -72,9 +74,8 @@ class Buttons extends React.PureComponent {
           >
             Show more
           </Button>
-        }
-        {
-          show_all_buttons &&
+        )}
+        {show_all_buttons && (
           <Button
             size="small"
             className={`ori-b-mrgn-5 ori-r-mrgn-5 ori-btn-bubble-inner ${styles.button}`}
@@ -82,7 +83,7 @@ class Buttons extends React.PureComponent {
           >
             Show less
           </Button>
-        }
+        )}
       </div>
     )
   }
@@ -98,7 +99,7 @@ Buttons.propTypes = {
 }
 
 Buttons.defaultProps = {
-  className: "",
+  className: '',
   btn_disabled: false,
   display_count: 4
 }
